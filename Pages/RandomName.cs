42c1eeac50caddef3b5c5ebaf4020416ccbd45cf
mrSashaman_ <psasha2811@gmail.com>
2026-06-01 20:18:47 +1200

    ListsUpdate

--- Pages/RandomName.cs
+++ Pages/RandomName.cs
@@ -7,10 +7,27 @@ namespace RandomFact.Pages;
 
 public class RandomName : PageModel
 {
-    private readonly List<string> _names = new()
-    {
-        "Александр", "Олег", "Мирослав", "Ленин", "Витя", "Платон", "Алексей"
-    };
+private readonly List<string> _names = new()
+{
+    "Александр", "Олег", "Мирослав", "Витя", "Платон", "Алексей",
+    "Иван", "Дмитрий", "Сергей", "Андрей", "Николай",
+    "Михаил", "Владимир", "Василий", "Павел", "Егор",
+    "Константин", "Юрий", "Борис", "Григорий", "Фёдор",
+    "Степан", "Антон", "Максим", "Виктор", "Валерий",
+    "John", "James", "Michael", "William", "Robert",
+    "David", "Richard", "Thomas", "Joseph", "Charles",
+    "Daniel", "Matthew", "Christopher", "Andrew", "Anthony",
+    "George", "Benjamin", "Samuel", "Jack", "Henry",
+    "Hans", "Fritz", "Karl", "Wilhelm", "Otto",
+    "Heinrich", "Erich", "Rudolf", "Klaus", "Wolfgang",
+    "Gustav", "Friedrich", "Johann", "Ludwig", "Walter",
+    "Werner", "Helmut", "Gerhard", "Konrad", "Dieter",
+    "Ленин", "Иосиф", "Карл", "Фридрих", "Энгельс",
+    "Маркс", "Лев", "Троцкий", "Мао", "Эрнесто",
+    "Фидель", "Хо", "Димитров", "Че", "Ким",
+    "Владлен", "Нинель", "Мэлс", "Ким", "Рэм",
+    "Октябрин", "Октябрина", "Вилен", "Марлен", "Даздраперм"
+};
 
     private readonly Random _random = new();
 
